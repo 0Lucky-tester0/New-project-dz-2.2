5f077593b371b4e1dbff985e3e7a1cb4e2590745
Emma Paris <Egorsovetskiy88@gmail.com>
2023-06-13 22:51:17 +0300

    a commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         int price = 1200; // сумма пополнения
         int check = 300; // счет клиента
         int ratio = 100; // сумма пополнения за 1 рубль бонуса
-        int bonus;
+        int bonus= price / ratio;
         if (price > 1000) {
-            System.out.println(bonus = (price / ratio);
+            System.out.println(bonus + price + check);
 
 
         }
