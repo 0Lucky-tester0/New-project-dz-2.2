e122651028a5d1a3832f6ff7624dc6103808d41d
Emma Paris <Egorsovetskiy88@gmail.com>
2023-06-13 21:24:44 +0300

    r commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int total; // итоговая сумма счета 
         int price = 1200; // сумма пополнения
-        if (price > 1000) {
-
-            int check = 300; // счет клиента
-            int coefficient = 100; // сумма пополнения за 1 рубль бонуса
-            total = (check + price) / coefficient;
-            System.out.println(total + check + price);
+        int check = 300; // счет клиента
+        int coefficient = 100; // сумма пополнения за 1 рубль бонуса
+       
 
         }
     }
